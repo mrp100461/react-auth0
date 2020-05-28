92e47feceb3bbd63584f9cdd5d8698ce55f3e0a5
mrp100461 <mrp100461@btinternet.com>
2020-05-28 21:42:07 +0100

    fix for private api call

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
 const express = require("express");
 require("dotenv").config();
-const jwt=require("express-jwt");
-const jwkRsa=require("jwks-rsa");
+const jwt = require("express-jwt"); // Validate JWT and set req.user
+const jwksRsa = require("jwks-rsa"); // Retrieve RSA keys from a JSON Web Key set (JWKS) endpoint
 
 const checkJwt = jwt({
-  secret: jwkRsa.expressJwtSecret({
-    cache: true,
+  // Dynamically provide a signing key based on the kid in the header
+  // and the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true, // cache the signing key
     rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.REACT_APP_AUTHO_DOMAIN}/.well-known/jwks.json`,
+    jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per minute
+    jwksUri: `https://${
+      process.env.REACT_APP_AUTH0_DOMAIN
+      }/.well-known/jwks.json`
   }),
-  audience: process.env.REACT_APP_AUTHO_AUDIENCE,
-  issuer:`https://${process.env.REACT_APP_AUTHO_DOMAIN}/`,
-  algorithms:["RS256"]
+
+  // Validate the audience and the issuer.
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
+
+  // This must match the algorithm selected in the Auth0 dashboard under your app's advanced settings under the OAuth tab
+  algorithms: ["RS256"]
 });
+
 const app = express();
 
 app.get("/public", function (req, res) {
   res.json({
-    message: "Hello from a public API!",
+    message: "Hello from a public API!"
   });
 });
-app.get("/private",checkJwt, function (req, res) {
+
+app.get("/private", checkJwt, function (req, res) {
   res.json({
-    message: "Hello from a private API!",
+    message: "Hello from a private API!"
   });
 });
+
 app.listen(3001);
 console.log("API server listening on " + process.env.REACT_APP_API_URL);
--- src/Private.js
+++ src/Private.js
@@ -2,25 +2,24 @@ import React, { Component } from "react";
 
 class Private extends Component {
   state = {
-    message: "",
+    message: ""
   };
+
   componentDidMount() {
     fetch("/private", {
-      headers: { Authorization: `Bearer $(this.props.auth.getAccessToken()` },
+      headers: { Authorization: `Bearer ${this.props.auth.getAccessToken()}` }
     })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("Network response was not ok");
+      .then(response => {
+        if (response.ok) return response.json();
+        throw new Error("Network response was not ok.");
       })
-      .then((response) => {
-        this.setState({ message: response.message });
-      })
-      .catch((error) => this.setState({ message: error.message }));
+      .then(response => this.setState({ message: response.message }))
+      .catch(error => this.setState({ message: error.message }));
   }
+
   render() {
     return <p>{this.state.message}</p>;
   }
 }
+
 export default Private;
